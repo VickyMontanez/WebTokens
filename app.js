eaaf23c928488ecf6cb050e9df4c772a186b48a8
VickyMontanezCampus <vmontanez707@gmail.com>
2023-07-18 08:27:29 -0500

    feat: :memo: Add Webtokens documentation
    Add Webtokens documentation

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-import "reflect-metadata";
-import {plainToClass} from "class-transformer";
-import {user} from "./controller/user.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import {SignJWT, jwtVerify} from 'jose';
+dotenv.config();
+const appExpress = express();
 
-let json = {
-    id: 12666,
-    nombre: "vickysiitaa",
-    edad: 17
-};
+appExpress.get("/:id/:nombre", async(req,res)=>{
 
-let data = plainToClass(user, json, {excludeExtraneousValues: true});
-console.log(data.nombreId);
+    let json = {
+        id: req.params.id,
+        nombre: req.params.nombre
+    }
+
+    const encoder = new TextEncoder();
+    const jwtconsructor = new SignJWT({json});
+    const jwt = await jwtconsructor
+    .setProtectedHeader({alg:"HS256", typ:"JWT"})
+    .setIssuedAt()
+    .setExpirationTime("60s")
+    .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+    res.send({jwt})
+});
+
+appExpress.post("/", async(req, res)=>{
+    const {authorization} = req.headers;
+    if(!authorization) return res.status(401).send({token: ":("});
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(
+            authorization,
+            encoder.encode(process.env.JWT_PRIVATE_KEY)
+        );
+        res.send(jwtData);
+    } catch (error) {
+        res.status(401).send({token: "Algo Salio Mal :("})
+    }
+})
+
+appExpress.listen(5010, ()=>{
+    console.log('http://localhost:5010/');
+})
